93d8289672aceca1dd9fbfdcafce446fe2c654eb
Vedesh S Khatri <khatrivedesh@gmail.com>
2025-06-30 23:15:54 +0530

    Add files

---
+++ dict 2.py
@@ -0,0 +1,5 @@
+#You are given a list of subjects for students. Assume one classroom is required for 1 subject. How many classrooms are needed by all students.
+# ”python”, “java”, “C++”,“C”,“python”,“javascript”,“java”,“python”,“java”,“C++”,“C”
+
+subjects = {"python", "java", "C++", "C", "python", "javascript", "java", "python", "java", "C++", "C"}
+print(subjects)
